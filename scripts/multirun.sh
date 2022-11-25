597f89b3ec64488b4726d5d733797fa360d88966
Timothée Mazzucotelli <pawamoy@pm.me>
2022-11-25 16:03:00 +0100

    chore: Don't check/test on Python 3.7 (still supported)

--- scripts/multirun.sh
+++ scripts/multirun.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-PYTHON_VERSIONS="${PYTHON_VERSIONS-3.7 3.8 3.9 3.10 3.11}"
+PYTHON_VERSIONS="${PYTHON_VERSIONS-3.8 3.9 3.10 3.11}"
 
 restore_previous_python_version() {
     if pdm use -f "$1" &>/dev/null; then
