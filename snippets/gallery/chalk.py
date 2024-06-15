e6eb0bb03141be62f235811ad56c617fd0d22db9
Timothée Mazzucotelli <dev@pawamoy.fr>
2024-06-15 16:19:42 +0200

    Deployed cc36267 with MkDocs version: 1.6.0

--- snippets/gallery/chalk.py
+++ snippets/gallery/chalk.py
@@ -1,5 +1,6 @@
+from tempfile import NamedTemporaryFile
+from chalk import Diagram, triangle, unit_x
 from colour import Color
-from chalk import Diagram, triangle
 
 papaya = Color("#ff9700")
 
@@ -8,7 +9,15 @@ def sierpinski(n: int, size: int) -> Diagram:
         return triangle(size)
     else:
         smaller = sierpinski(n - 1, size / 2)
-        return smaller.above(smaller.beside(smaller).center_xy())
+        return smaller.above(smaller.beside(smaller, unit_x).center_xy())
 
 d = sierpinski(5, 4).fill_color(papaya)
-d.render()
+
+# Chalk doesn't provide an easy method to get a string directly,
+# so we use a temporary file.
+with NamedTemporaryFile("w+") as tmpfile:
+    d.render_svg(tmpfile.name, height=256)
+    tmpfile.seek(0)
+    svg = tmpfile.read()
+
+print(svg)
