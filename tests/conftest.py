de768a549badacb28672dd26bcf41c15ec93fc09
Ben Burns <803016+benjamincburns@users.noreply.github.com>
2025-02-07 19:49:05 +1300

    shutdown harder

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,12 @@
 """Configuration for the pytest test suite."""
 
+from collections.abc import Iterable
+
 import pytest
 from markdown import Markdown
 
 from markdown_exec import formatter, formatters, validator
+from markdown_exec.formatters.jupyter import _shutdown_kernels
 
 
 @pytest.fixture
@@ -26,3 +29,9 @@ def md() -> Markdown:
         extensions=["pymdownx.superfences", "pymdownx.tabbed"],
         extension_configs={"pymdownx.superfences": {"custom_fences": fences}},
     )
+
+@pytest.fixture(autouse=True)
+def cleanup() -> Iterable[None]:
+    """Cleanup the test environment."""
+    yield
+    _shutdown_kernels()
