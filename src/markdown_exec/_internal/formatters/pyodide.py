784f1cee4aa84246d627c2029189a88f8343682c
Timothée Mazzucotelli <dev@pawamoy.fr>
2026-07-04 17:42:11 +0200

    wip

--- src/markdown_exec/_internal/formatters/pyodide.py
+++ src/markdown_exec/_internal/formatters/pyodide.py
@@ -15,14 +15,6 @@ _play_emoji = (
 )
 _clear_emoji = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M15.14 3c-.51 0-1.02.2-1.41.59L2.59 14.73c-.78.77-.78 2.04 0 2.83L5.03 20h7.66l8.72-8.73c.79-.77.79-2.04 0-2.83l-4.85-4.85c-.39-.39-.91-.59-1.42-.59M17 18l-2 2h7v-2"></path></svg>'
 
-_assets = """
-<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/ace/1.16.0/ace.js"></script>
-<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.7.0/highlight.min.js"></script>
-<script type="text/javascript" src="https://cdn.jsdelivr.net/pyodide/v{version}/full/pyodide.js"></script>
-<link title="light" rel="alternate stylesheet" href="https://cdn.jsdelivr.net/npm/highlightjs-themes@1.0.0/tomorrow.min.css" disabled="disabled">
-<link title="dark" rel="alternate stylesheet" href="https://cdn.jsdelivr.net/npm/highlightjs-themes@1.0.0/tomorrow-night-blue.min.css" disabled="disabled">
-"""
-
 _template = """
 <div class="pyodide">
 <div class="pyodide-editor-bar">
@@ -34,20 +26,6 @@ _template = """
 </div>
 <pre><code id="%(id_prefix)soutput" class="pyodide-output"></code></pre>
 </div>
-
-<script>
-document.addEventListener('DOMContentLoaded', (event) => {
-    setupPyodide(
-        '%(id_prefix)s',
-        install=%(install)s,
-        themeLight='%(theme_light)s',
-        themeDark='%(theme_dark)s',
-        session='%(session)s',
-        minLines=%(min_lines)s,
-        maxLines=%(max_lines)s,
-    );
-});
-</script>
 """
 
 _counter = 0
@@ -73,7 +51,6 @@ def _format_pyodide(code: str, md: Markdown, session: str, extra: dict, **option
     global _counter  # noqa: PLW0603
     _counter += 1
 
-    version = extra.pop("version", "0.26.4").lstrip("v")
     install = extra.pop("install", "")
     install = install.split(",") if install else []
     exclude_assets = extra.pop("assets", "1").lower() in {"0", "false", "no", "off"}
@@ -98,4 +75,4 @@ def _format_pyodide(code: str, md: Markdown, session: str, extra: dict, **option
     rendered = _template % data
     if exclude_assets:
         return rendered
-    return _assets.format(version=version) + rendered
+    return rendered
